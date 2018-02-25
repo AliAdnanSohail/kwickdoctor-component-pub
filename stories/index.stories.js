e311ef8824cea1c892de6a0aef4660a0474fb375
Pavel Pauliushchyk <p.pavliushchik@aisnovations.com>
2018-02-26 01:18:20 +0300

    storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Welcome } from '@storybook/react/demo';
+
+import Button from '../src/components/Button';
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')} title="Hello Button" />
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')} title="😀 😎 👍 💯" />
+  ));
