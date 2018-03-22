bf921a2be04bfd77d9cac357d107df55858e3167
Eugene <eugene@wellyes.ru>
2018-03-22 19:05:41 +0300

    - Added tag input

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as Card } from './components/Card';
 export { default as TextInput } from './components/TextInput';
 export { default as Toast } from './components/Toast';
 export { default as ButtonGroup } from './components/ButtonGroup';
+export { default as TagInput } from './components/TagInput';
---
+++ stories/TagInput.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { checkA11y } from '@storybook/addon-a11y';
+import { withTests } from '@storybook/addon-jest';
+
+import { TagInput } from '../src';
+import results from '../.jest-test-results.json';
+
+storiesOf('TagInput', module)
+  .addDecorator(withKnobs)
+  .addDecorator(checkA11y)
+  .addDecorator(withTests({ results })('TagInput'))
+  .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
+  .add('Tag input', () => (
+    <TagInput
+      // id="text-input"
+      // name={text('name', 'name')}
+      // onChange={action('changed')}
+      // placeholder={text('placeholder', 'text input')}
+      // value="value"
+      // label="text input"
+    />
+  ));
