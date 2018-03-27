c600336557198c29248c1ba1d9c94f5b6178a85b
Ignatovich Mikhail <m.s.ignat@gmail.com>
2018-03-27 13:53:55 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ export { default as Card } from './components/Card';
 export { default as Datepicker } from './components/Datepicker';
 export { default as TextInput } from './components/TextInput';
 export { default as Toast } from './components/Toast';
-<<<<<<< HEAD
-export { default as ButtonGroup } from './components/ButtonGroup';
 export { default as AvatarPicker } from './components/AvatarPicker';
-=======
->>>>>>> master
