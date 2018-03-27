22fff8231ffbf75089852d66a10ab0a3b5067fcf
Ignatovich Mikhail <m.s.ignat@gmail.com>
2018-03-27 16:48:17 +0300

    Increasing test coverage

--- __tests__/FileInput.test.js
+++ __tests__/FileInput.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
 import { FileInput } from '../src';
 
 it('renders correctly', () => {
-  const element = renderer.create(<FileInput>test</FileInput>).toJSON();
+  const element = renderer.create(<FileInput id="test">test</FileInput>).toJSON();
 
   expect(element).toMatchSnapshot();
+});
+it('input change correct', () => {
+  const wrapper = shallow(<FileInput id="test" />);
+  console.log(wrapper.debug());
+  wrapper.find('input').simulate('change', {
+    target: {
+      files: [
+        'someimage.png',
+      ],
+    },
+  });
 });
