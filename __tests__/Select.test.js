d1b2de9770a0ac5dc6c03a70ecfcad04d79b36d7
danil <komarov.fl@yandex.ru>
2018-04-09 12:10:13 +0300

    [Input] tests added

---
+++ __tests__/Select.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Select } from '../src';
+
+const defaultProps = {
+  id: 'input-id',
+  name: 'input-name',
+  label: 'input-label',
+  placeholder: 'input-placeholder',
+};
+
+it('Select renders correctly', () => {
+  const element = renderer.create((
+    <Select {...defaultProps}>
+      <option value="1" />
+      <option value="2" />
+      <option value="3" />
+      <option value="4" />
+    </Select>
+  )).toJSON();
+
+  expect(element).toMatchSnapshot();
+});
--- stories/FormFields.stories.js
+++ stories/FormFields.stories.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 import { checkA11y } from '@storybook/addon-a11y';
-// import { withTests } from '@storybook/addon-jest';
+import { withTests } from '@storybook/addon-jest';
 
 import { Input, TextArea, Select } from '../src';
-// import results from '../.jest-test-results.json';
+import results from '../.jest-test-results.json';
 
 storiesOf('Form Fields', module)
   .addDecorator(withKnobs)
   .addDecorator(checkA11y)
-  // .addDecorator(withTests({ results })(''))
+  .addDecorator(withTests({ results })('Input'))
   .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
   .add('Input', () => (
     <Input
@@ -19,7 +19,13 @@ storiesOf('Form Fields', module)
       label={text('label', 'Label')}
       placeholder={text('placeholder', 'Start typing')}
     />
-  ))
+  ));
+
+storiesOf('Form Fields', module)
+  .addDecorator(withKnobs)
+  .addDecorator(checkA11y)
+  .addDecorator(withTests({ results })('Textarea'))
+  .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
   .add('TextArea', () => (
     <TextArea
       id="textarea"
@@ -27,7 +33,13 @@ storiesOf('Form Fields', module)
       label={text('label', 'Label')}
       placeholder={text('placeholder', 'Start typing')}
     />
-  ))
+  ));
+
+storiesOf('Form Fields', module)
+  .addDecorator(withKnobs)
+  .addDecorator(checkA11y)
+  .addDecorator(withTests({ results })('Select'))
+  .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
   .add('Select', () => (
     <Select>
       <option value="1" />
