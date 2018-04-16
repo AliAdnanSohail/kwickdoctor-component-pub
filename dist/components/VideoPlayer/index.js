b3e1169235e35943a2fe01515d24643bbe347005
Pavel Pauliushchyk <p.pavliushchik@aisnovations.com>
2018-04-16 17:40:13 +0300

    build

--- dist/components/VideoPlayer/index.js
+++ dist/components/VideoPlayer/index.js
@@ -7,33 +7,19 @@ exports.default = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _style = require('styled-jsx/style');
+
+var _style2 = _interopRequireDefault(_style);
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
-
-var _classnames = require('classnames');
-
-var _classnames2 = _interopRequireDefault(_classnames);
 
 var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-var _css = require('styled-jsx/css');
-
-var _css2 = _interopRequireDefault(_css);
-
-var _Slider = require('./Slider');
-
-var _Slider2 = _interopRequireDefault(_Slider);
-
-var _FullScreenButton = require('./FullScreenButton');
-
-var _FullScreenButton2 = _interopRequireDefault(_FullScreenButton);
-
-var _PlayPauseButton = require('./PlayPauseButton');
-
-var _PlayPauseButton2 = _interopRequireDefault(_PlayPauseButton);
+var _videoReact = require('video-react');
 
 var _styles = require('./styles');
 
@@ -50,161 +36,51 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var VideoPlayer = function (_Component) {
   _inherits(VideoPlayer, _Component);
 
-  function VideoPlayer(props) {
+  function VideoPlayer() {
     _classCallCheck(this, VideoPlayer);
 
-    var _this = _possibleConstructorReturn(this, (VideoPlayer.__proto__ || Object.getPrototypeOf(VideoPlayer)).call(this, props));
-
-    _this.getVideoRef = function (video) {
-      _this.videoRef = video;
-      _this.initVideo();
-    };
-
-    _this.getVideoContainerRef = function (elem) {
-      _this.containerRef = elem;
-    };
-
-    _this.setVideoProgress = function (progress) {
-      if (_this.videoRef) {
-        var time = Math.round(progress / 100 * _this.videoRef.duration);
-        if (_this.videoRef.currentTime !== time) {
-          _this.videoRef.currentTime = time;
-        }
-      }
-    };
-
-    _this.calcVideoProgress = function () {
-      var value = 0;
-      if (_this.videoRef) {
-        value = Math.round(100 / _this.videoRef.duration * _this.videoRef.currentTime);
-        _this.setState({ videoProgress: value });
-      }
-      return value;
-    };
-
-    _this.initVideo = function () {
-      if (_this.videoRef) {
-        _this.setState({ playingVideo: !_this.state.playingVideo });
-        _this.videoRef.loop = true;
-        // this.videoRef.muted = true;
-        console.log(_this.videoRef.duration);
-        _this.videoRef.ontimeupdate = _this.calcVideoProgress;
-        _this.playPromise = _this.videoRef.play();
-      }
-    };
-
-    _this.pauseVideo = function () {
-      if (_this.playPromise !== undefined) {
-        _this.playPromise.then(function () {
-          _this.videoRef.pause();
-        }).catch(function (error) {
-          console.error(error);
-        });
-      }
-    };
-
-    _this.checkFullScreenSupport = function () {
-      if ('fullscreenEnabled' in document || 'webkitFullscreenEnabled' in document || 'mozFullScreenEnabled' in document || 'msFullscreenEnabled' in document) {
-        if (document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled) {
-          return true;
-        }
-      }
-      return false;
-    };
-
-    _this.makeAllControlsVIsible = function () {};
-
-    _this.playFullScreen = function () {
-      // check if user allows full screen of elements. This can be enabled or disabled in browser config. By default its enabled.
-      // its also used to check if browser supports full screen api.
-      if (_this.checkFullScreenSupport()) {
-        // requestFullscreen is used to display an this.videoRef in full screen mode.
-        if ('requestFullscreen' in _this.containerRef) {
-          _this.containerRef.requestFullscreen();
-        } else if ('webkitRequestFullscreen' in _this.containerRef) {
-          _this.containerRef.webkitRequestFullscreen();
-        } else if ('mozRequestFullScreen' in _this.containerRef) {
-          _this.containerRef.mozRequestFullScreen();
-        } else if ('msRequestFullscreen' in _this.containerRef) {
-          _this.containerRef.msRequestFullscreen();
-        }
-        _this.containerRef.controls = false;
-      } else {
-        console.log("User doesn't allow full screen");
-      }
-    };
-
-    _this.screenChange = function () {
-      // fullscreenElement is assigned to html element if any element is in full screen mode.
-      if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-        console.log('Current full screen element is : ' + (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement));
-      } else {
-        // exitFullscreen us used to exit full screen manually
-        if ('exitFullscreen' in document) {
-          document.exitFullscreen();
-        } else if ('webkitExitFullscreen' in document) {
-          document.webkitExitFullscreen();
-        } else if ('mozCancelFullScreen' in document) {
-          document.mozCancelFullScreen();
-        } else if ('msExitFullscreen' in document) {
-          document.msExitFullscreen();
-        }
-      }
-    };
-
-    _this.playVideo = function () {
-      _this.playPromise = _this.videoRef.play();
-    };
-
-    _this.togglePlayPause = function () {
-      if (_this.state.playing) {
-        _this.pauseVideo();
-        _this.setState({ playing: false });
-      } else {
-        _this.playVideo();
-        _this.setState({ playing: true });
-      }
-    };
-
-    _this.state = {
-      playingVideo: false,
-      videoProgress: 0,
-      playing: true
-    };
-    _this.checkFullScreenSupport();
-    return _this;
+    return _possibleConstructorReturn(this, (VideoPlayer.__proto__ || Object.getPrototypeOf(VideoPlayer)).apply(this, arguments));
   }
 
   _createClass(VideoPlayer, [{
     key: 'render',
     value: function render() {
-      var _this2 = this;
+      var _props = this.props,
+          muted = _props.muted,
+          src = _props.src;
 
-      // this.getMediaDevices();
+
       return _react2.default.createElement(
         'div',
-        { className: 'videoPlaycer_container', ref: this.getVideoContainerRef },
-        _react2.default.createElement(_FullScreenButton2.default, { className: 'fullscreen_button', onClick: function onClick() {
-            return _this2.playFullScreen();
-          } }),
-        _react2.default.createElement('video', { ref: this.getVideoRef, src: this.props.src }),
+        {
+          className: 'jsx-' + _styles2.default.__scopedHash + ' ' + 'video-player-container'
+        },
         _react2.default.createElement(
-          'div',
-          { className: 'videoPlayer_controls' },
-          _react2.default.createElement(_Slider2.default, {
-            value: this.state.videoProgress,
-            onChange: function onChange(value) {
-              _this2.setState({ videoProgress: value });
-              _this2.setVideoProgress(value);
-            },
-            onMousedown: this.pauseVideo,
-            onMouseup: this.playVideo
-          })
+          _videoReact.Player,
+          { playsInline: true, fluid: true, muted: muted, className: 'video-player' },
+          _react2.default.createElement('source', { src: src, className: 'jsx-' + _styles2.default.__scopedHash
+          }),
+          _react2.default.createElement(
+            _videoReact.ControlBar,
+            { className: 'video-player__control-bar' },
+            _react2.default.createElement(_videoReact.PlayToggle, { disabled: true }),
+            _react2.default.createElement(_videoReact.VolumeMenuButton, { disabled: true }),
+            _react2.default.createElement(_videoReact.CurrentTimeDisplay, { disabled: true }),
+            _react2.default.createElement(_videoReact.TimeDivider, { disabled: true }),
+            _react2.default.createElement(_videoReact.DurationDisplay, { disabled: true }),
+            _react2.default.createElement(_videoReact.ProgressControl, { className: 'video-player__progress', order: 1.1 }),
+            _react2.default.createElement(_videoReact.FullscreenToggle, { className: 'video-player__fullscreen-button', order: 1.2 })
+          ),
+          _react2.default.createElement(_videoReact.BigPlayButton, { position: 'center', className: 'video-player__big-play-button', order: 2 })
         ),
+        _react2.default.createElement(_style2.default, {
+          styleId: _styles2.default.__scopedHash,
+          css: _styles2.default.__scoped
+        }),
         _react2.default.createElement(
           'style',
           null,
-          _styles2.default
+          _styles.videoPlayer
         )
       );
     }
@@ -217,6 +93,10 @@ exports.default = VideoPlayer;
 
 
 VideoPlayer.propTypes = {
+  muted: _propTypes2.default.bool,
   src: _propTypes2.default.string.isRequired
 };
-// onChange={(event) => { this.setVideoProgress(event.target.value); }}
+
+VideoPlayer.defaultProps = {
+  muted: false
+};
