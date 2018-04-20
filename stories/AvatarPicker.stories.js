665ef1ae74495fb80f0ccf015901c6e54312bd35
diana_mkr <d.makerevich@aisnovations.com>
2018-04-20 18:29:58 +0300

    change default avatar picker, delete other

--- stories/AvatarPicker.stories.js
+++ stories/AvatarPicker.stories.js
@@ -16,34 +16,7 @@ storiesOf('Avatar Picker', module)
   .add('default', () => (
     <AvatarPicker
       id="avatar"
-      name={text('name', 'name')}
-      onChange={action('onChange')}
-    />
-  ));
-
-storiesOf('Avatar Picker', module)
-  .addDecorator(withKnobs)
-  .addDecorator(checkA11y)
-  .addDecorator(withTests({ results })('AvatarPicker'))
-  .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
-  .add('big', () => (
-    <AvatarPicker
-      id="avatar"
-      size="big"
-      name={text('name', 'name')}
-      onChange={action('onChange')}
-    />
-  ));
-
-storiesOf('Avatar Picker', module)
-  .addDecorator(withKnobs)
-  .addDecorator(checkA11y)
-  .addDecorator(withTests({ results })('AvatarPicker'))
-  .addDecorator(getStory => <div style={{ padding: '24px' }}>{getStory()}</div>)
-  .add('medium', () => (
-    <AvatarPicker
-      id="avatar"
-      size="medium"
+      size={text('size', '')}
       name={text('name', 'name')}
       onChange={action('onChange')}
     />
