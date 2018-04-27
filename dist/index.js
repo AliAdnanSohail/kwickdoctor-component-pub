d7bbf34435ac08b6381b424bd378ae9d87a3e7cb
aliaksandr-s <s-a-n-9@mail.ru>
2018-04-27 11:10:05 +0300

    Update build

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Validate = undefined;
+exports.Validate = exports.Icons = undefined;
 
 var _components = require('./components');
 
@@ -28,4 +28,12 @@ Object.defineProperty(exports, 'Validate', {
 
 require('grommet/grommet.min.css');
 
+var _icons = require('grommet/components/icons');
+
+var _Icons = _interopRequireWildcard(_icons);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Icons = _Icons;
