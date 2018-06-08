1006cc6617dffb345357e5045434a490d2e0d471
Diana <d.makerevich@aisnovations.com>
2018-06-08 14:19:15 +0300

    fixed notification

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { CheckmarkIcon, TrashIcon } from 'grommet/components/icons';
 import classnames from 'classnames';
 import moment from 'moment';
-import { CheckmarkIcon, TrashIcon } from 'grommet/components/icons';
+import PropTypes from 'prop-types';
 
 import { Button, Card } from '../';
 import { notification, notificationCard } from './styles';
 
 export default class Notification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonsVisible: false,
+    };
+  }
+
+  handleShowButtons = () => {
+    this.setState({ buttonsVisible: !this.state.buttonsVisible });
+  };
+
   renderDate = () => moment(this.props.date).format('h:mm A, MMM D');
 
   render() {
@@ -30,8 +41,51 @@ export default class Notification extends Component {
         </div>
 
         <div className="notification__actions">
-          <Button flat icon={<CheckmarkIcon />} onClick={onComplete} rounded size="s" />
-          <Button flat icon={<TrashIcon />} onClick={onDelete} rounded size="s" />
+          <Button
+            flat
+            icon={<CheckmarkIcon />}
+            onClick={onComplete}
+            rounded
+            size="s"
+          />
+
+          <Button
+            flat
+            icon={<TrashIcon />}
+            onClick={onDelete}
+            rounded
+            size="s"
+          />
+
+        </div>
+
+        <div className="notification__actions--mobile">
+          <button className="notification__points" onClick={this.handleShowButtons}>
+            <span className="notification__point" />
+            <span className="notification__point" />
+            <span className="notification__point" />
+          </button>
+          {this.state.buttonsVisible && (
+            <div className="notification__buttons">
+              <Button
+                flat
+                icon={<CheckmarkIcon />}
+                onClick={onComplete}
+                size="s"
+              >
+                Read
+              </Button>
+
+              <Button
+                flat
+                icon={<TrashIcon />}
+                onClick={onDelete}
+                size="s"
+              >
+                Delete
+              </Button>
+            </div>
+          )}
         </div>
 
         <style jsx>{notification}</style>
@@ -40,7 +94,7 @@ export default class Notification extends Component {
     );
   }
 }
-
+ 
 Notification.propTypes = {
   date: PropTypes.object.isRequired,
   message: PropTypes.string.isRequired,
--- src/components/Notification/styles.js
+++ src/components/Notification/styles.js
@@ -42,126 +42,126 @@ export const notification = css`
             border-radius: 50%;
             background-color: ${settings.textColor};
     
-        &--primary {
-            background-color: ${settings.primaryColor};
-        }
-    
-        &--read {
-            opacity: 0.5;
-        }
-    }
- 
-    &__message {
-        flex: 1;
-        display: flex;
-        align-items: center;
-        padding: 0 16px;
-        font-size: 0.875rem;
-        color: ${settings.textColor};
-    }
- 
-    &__actions {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        padding: 0 24px;
-
-        :global(svg) {
-            fill: ${settings.textLightColor};
-            stroke: ${settings.textLightColor};
- 
-            :global(path, circle, polyline) {
-                fill: none;
-                stroke: ${settings.textLightColor}!important;
+            &--primary {
+                background-color: ${settings.primaryColor};
             }
-        }
- 
-        &--mobile {
-            display: none;
-        }
-    }
- 
-    @media (max-width: 960px) {
-        &__time {
-            font-size: 14px;
-            padding: 15px;
-        }
-    }
-
-    @media (max-width: 790px) {
-        :global(.button--flat) {
-            color: #bbbccd;
-        }
- 
-        &__points {
-            border: none;
-            background: none;
-            outline: none;
-            display: flex;
-            justify-content: flex-end;
-            padding: 10px;
-            position: absolute;
-            right: 15px;
-            top: 15px;
-        }
- 
-        &__point {
-            display: block;
-            width: 8px;
-            height: 8px;
-            background-color: #bbbccd;
-            border-radius: 50%;
-            margin: 3px;
-        }
- 
-        &__buttons {
-            display: flex;
-            flex-direction: column;
-            position: absolute;
-            right: 0;
-            top: 40px;
-            box-shadow: rgba(0, 0, 0, 0.1) 0px 12px 34px;
-            background: rgb(255, 255, 255);
-            border-radius: 4px;
-        }
-
-        &-card {
-            flex-direction: column-reverse;
-        }
- 
-        &__time {
-            width: 100%;
-            border-bottom: none;
-            border-right: none;
-            justify-content: flex-start;
-            padding: 24px;
-            font-weight: 100;
-            padding-top: 0;
+        
+            &--read {
+                opacity: 0.5;
+            }
         }
  
         &__message {
-            width: 100%;
-            min-width: 100%;
-            padding: 24px;
-            text-align: left;
+            flex: 1;
             display: flex;
-            align-items: flex-start;
-            box-sizing: border-box;
-            border-bottom: none;
-        }
- 
-        &__dot {
-            display: block;
-            margin-top: 4px;
+            align-items: center;
+            padding: 0 16px;
+            font-size: 0.875rem;
+            color: ${settings.textColor};
         }
  
         &__actions {
-            display: none;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 0 24px;
+
+            :global(svg) {
+                fill: ${settings.textLightColor};
+                stroke: ${settings.textLightColor};
+    
+                :global(path, circle, polyline) {
+                    fill: none;
+                    stroke: ${settings.textLightColor}!important;
+                }
+            }
+    
             &--mobile {
-            display: block;
-            position: relative;
+                display: none;
+            }
+        }
+    
+        @media (max-width: 960px) {
+            &__time {
+                font-size: 14px;
+                padding: 15px;
+            }
+        }
+
+        @media (max-width: 790px) {
+            :global(.button--flat) {
+                color: #bbbccd;
+            }
+    
+            &__points {
+                border: none;
+                background: none;
+                outline: none;
+                display: flex;
+                justify-content: flex-end;
+                padding: 10px;
+                position: absolute;
+                right: 15px;
+                top: 15px;
+            }
+    
+            &__point {
+                display: block;
+                width: 8px;
+                height: 8px;
+                background-color: #bbbccd;
+                border-radius: 50%;
+                margin: 3px;
+            }
+    
+            &__buttons {
+                display: flex;
+                flex-direction: column;
+                position: absolute;
+                right: 0;
+                top: 40px;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 12px 34px;
+                background: rgb(255, 255, 255);
+                border-radius: 4px;
+            }
+
+            &-card {
+                flex-direction: column-reverse;
+            }
+    
+            &__time {
+                width: 100%;
+                border-bottom: none;
+                border-right: none;
+                justify-content: flex-start;
+                padding: 24px;
+                font-weight: 100;
+                padding-top: 0;
+            }
+    
+            &__message {
+                width: 100%;
+                min-width: 100%;
+                padding: 24px;
+                text-align: left;
+                display: flex;
+                align-items: flex-start;
+                box-sizing: border-box;
+                border-bottom: none;
+            }
+    
+            &__dot {
+                display: block;
+                margin-top: 4px;
+            }
+    
+            &__actions {
+                display: none;
+                &--mobile {
+                    display: block;
+                    position: relative;
+                }
             }
         }
     }
-  }
 `;
