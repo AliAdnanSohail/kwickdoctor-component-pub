b35f7d17992f1e977ed13fe07b69b070381121f4
Igors <igors.gudelis@gmail.com>
2018-06-27 14:32:41 +0300

    build:VideoPlayer

--- dist/components/VideoPlayer/index.js
+++ dist/components/VideoPlayer/index.js
@@ -19,6 +19,10 @@ var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
 
+var _materialIconsReact = require('material-icons-react');
+
+var _materialIconsReact2 = _interopRequireDefault(_materialIconsReact);
+
 var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
@@ -52,7 +56,8 @@ var VideoPlayer = function (_Component) {
       var _props = this.props,
           containerClassName = _props.containerClassName,
           muted = _props.muted,
-          src = _props.src;
+          src = _props.src,
+          noVideoIconSize = _props.noVideoIconSize;
 
 
       return _react2.default.createElement(
@@ -60,7 +65,7 @@ var VideoPlayer = function (_Component) {
         {
           className: 'jsx-' + _styles2.default.__scopedHash + ' ' + ((0, _classnames2.default)('video-player-container', containerClassName) || '')
         },
-        _react2.default.createElement(
+        src && _react2.default.createElement(
           _videoReact.Player,
           { playsInline: true, fluid: true, muted: muted, className: 'video-player' },
           _react2.default.createElement('source', { src: src, className: 'jsx-' + _styles2.default.__scopedHash
@@ -78,6 +83,7 @@ var VideoPlayer = function (_Component) {
           ),
           _react2.default.createElement(_videoReact.BigPlayButton, { position: 'center', className: 'video-player__big-play-button', order: 2 })
         ),
+        !src && _react2.default.createElement(_materialIconsReact2.default, { color: '#BBBCCD', icon: 'videocam_off', size: noVideoIconSize }),
         _react2.default.createElement(_style2.default, {
           styleId: _styles2.default.__scopedHash,
           css: _styles2.default.__scoped
