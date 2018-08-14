c7d37723455d549633d7d33c6d5ac649957ffb81
Pavel Pauliushchyk <p.pavliushchik@aisnovations.com>
2018-08-14 18:25:51 +0300

    build

--- dist/components/VideoRecorder/styles.js
+++ dist/components/VideoRecorder/styles.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.unavailable = exports.countdown = exports.controls = undefined;
 
@@ -12,9 +12,9 @@ var _styleHelper2 = _interopRequireDefault(_styleHelper);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _defaultExport = ['.video-recorder{position:relative;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;overflow:hidden;width:100%;height:480px;background-color:' + _styleHelper2.default.borderColor + ';}', '@media (min-width:700px){.video-recorder{width:640px;}}'];
-_defaultExport.__hash = '576727000';
-_defaultExport.__scoped = ['.video-recorder.jsx-1166752729{position:relative;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;overflow:hidden;width:100%;height:480px;background-color:' + _styleHelper2.default.borderColor + ';}', '@media (min-width:700px){.video-recorder.jsx-1166752729{width:640px;}}'];
-_defaultExport.__scopedHash = '1166752729';
+_defaultExport.__hash = '4048917466';
+_defaultExport.__scoped = ['.video-recorder.jsx-3895914587{position:relative;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;overflow:hidden;width:100%;height:480px;background-color:' + _styleHelper2.default.borderColor + ';}', '@media (min-width:700px){.video-recorder.jsx-3895914587{width:640px;}}'];
+_defaultExport.__scopedHash = '3895914587';
 exports.default = _defaultExport;
 var controls = exports.controls = ['.video-recorder-controls{position:absolute;bottom:0;padding:24px;width:100%;box-sizing:border-box;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;}', '.video-recorder-controls p{margin-bottom:16px;color:#ffffff;font-size:0.875rem;text-transform:uppercase;-webkit-letter-spacing:0.2rem;-moz-letter-spacing:0.2rem;-ms-letter-spacing:0.2rem;letter-spacing:0.2rem;}'];
 
@@ -27,6 +27,6 @@ countdown.__hash = '3484136556';
 countdown.__scoped = ['.countdown.jsx-2150280269{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;height:76px;width:148px;border-radius:6px;background-color:rgba(255,255,255,0.4);color:#ffffff;font-size:1.5rem;font-weight:bold;}'];
 countdown.__scopedHash = '2150280269';
 var unavailable = exports.unavailable = ['.video-unavailable{height:480px;width:640px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:' + _styleHelper2.default.borderColor + ';font-size:1.5rem;color:' + _styleHelper2.default.textLightColor + ';}'];
-unavailable.__hash = '115934391';
-unavailable.__scoped = ['.video-unavailable.jsx-760928758{height:480px;width:640px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:' + _styleHelper2.default.borderColor + ';font-size:1.5rem;color:' + _styleHelper2.default.textLightColor + ';}'];
-unavailable.__scopedHash = '760928758';
+unavailable.__hash = '161998546';
+unavailable.__scoped = ['.video-unavailable.jsx-4132344403{height:480px;width:640px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:row;-ms-flex-direction:row;flex-direction:row;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;background-color:' + _styleHelper2.default.borderColor + ';font-size:1.5rem;color:' + _styleHelper2.default.textLightColor + ';}'];
+unavailable.__scopedHash = '4132344403';
